173204d3f9bd1c2d7043907a7f627a3c2e9806fd
Alan Paixao <alanpaixao2@gmail.com>
2022-07-06 23:08:22 -0300

    07 - questions inside elements

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -9,7 +9,7 @@ questions.forEach((question) => {
   btn.addEventListener("click", () => {
     questions.forEach((item) => {
       if (item !== question) {
-        item.classList.remove("show-text");
+        item.classList.remove("show-text ");
       }
     });
     question.classList.toggle("show-text");
